20d292576f2f055c823c9dfbea8156e91a7e91c8
RayWu222 <raymond.wu0114@gmail.com>
2024-02-23 00:15:02 -0800

    Update timeline data

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -10,7 +10,13 @@ export default[
         year:'2021',
         title:'Web Developer Inten',
         duration: '4 months',
-        details:"TBA"
+        details:"An internship with CodeDay where I collaborated with two other interns and an experienced project manager from Splunk to create  a climate change tracker. Project involve grabbing data from a 3rd party weather API and display visual data to the user using React, Node, and JavaScript and some TypeScript"
 
+    },
+    {
+        year:'2020',
+        title: 'React Native Developer Intern',
+        duration: '3 months',
+        details:"Mobile Web Project created using React Native. Using customer's Figma design to create a workable version of the app. Group Project, my role is mainly implementing Log in with Slack to the app for their 1000+ members"
     }
 ]
