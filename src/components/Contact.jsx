4274c14837130cd7051bdb3b250d67e5c0591de8
RayWu222 <raymond.wu0114@gmail.com>
2024-02-27 15:48:15 -0800

    Changed button type to submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,7 @@ function Contact(){
                             >
                         </textarea>
                         <button
-                            type = "button"
+                            type = "submit"
                             className = "text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
                             >
                             Work With Me
